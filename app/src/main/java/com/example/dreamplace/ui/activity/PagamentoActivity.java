46c0a28677f5b4cfc4944f48589c4142daa9bae4
Lucas Maranhão <Lucasvsm@gmail.com>
2022-05-10 20:02:01 -0300

    Criando o layout de pagamento

---
+++ app/src/main/java/com/example/dreamplace/ui/activity/PagamentoActivity.java
@@ -0,0 +1,35 @@
+package com.example.dreamplace.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.dreamplace.R;
+import com.example.dreamplace.model.Pacote;
+import com.example.dreamplace.util.MoedaUtil;
+
+import java.math.BigDecimal;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+}
